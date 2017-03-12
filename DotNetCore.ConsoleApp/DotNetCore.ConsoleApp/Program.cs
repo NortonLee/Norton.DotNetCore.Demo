d4347479600a20561a32bc4bd820adca9346083c
NortonLee <lizard@yeah.net>
2017-03-12 09:48:58 +0800

    init project

---
+++ DotNetCore.ConsoleApp/DotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCore.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
